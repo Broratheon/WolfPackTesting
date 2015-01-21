0d50356df156cc2f11f06108a96737184f37fa4b
Broratheon <hyungseo0107@gmail.com>
2015-01-20 23:02:59 -0500

    changed to using csv.parse

--- parser.js
+++ parser.js
@@ -1,6 +1,24 @@
 var parse_courses = function()
 {
-	testing = d3.csv("CourseLocations.csv")
+	if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            return xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET", "CourseLocations.csv", false );
+    xmlhttp.send();  
+
+	testing = d3.csv.parse(xmlhttp.responseText);
 
 	console.log(testing)
 
