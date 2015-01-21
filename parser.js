5c1403c9f391d74c87075bfa1c635f5c4f68aaa3
Broratheon <hyungseo0107@gmail.com>
2015-01-20 22:39:13 -0500

    testing function 3

--- parser.js
+++ parser.js
@@ -2,7 +2,7 @@ var parse_courses = function()
 {
 	var courses_array;
 
-	d3.csv("CourseLocations.csv", function(courses)
+	courses_array = d3.csv("CourseLocations.csv", function(courses)
 		{
 			courses_array = courses;
 		})
